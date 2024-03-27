6d383b8553511ad6623f20806215788a2906abef
jjustin-35 <jjusttin11@gmail.com>
2024-03-27 22:22:34 +0800

    update: remove prop-types

--- components/Message/index.tsx
+++ components/Message/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import propTypes from "prop-types";
 import { MessageText } from "./styled";
 
 const Message = ({ children }: { children: React.ReactNode }) => (
   <MessageText>{children}</MessageText>
 );
-
-Message.propTypes = {
-  children: propTypes.string,
-};
 
 export default Message;
